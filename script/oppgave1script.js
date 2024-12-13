6c81254f3140c05cbbceb33e029b890ce73ebbae
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-13 19:54:42 +0100

    Fixed hints for task 1

--- script/oppgave1script.js
+++ script/oppgave1script.js
@@ -1,11 +1,21 @@
+let wrongCounter = 0;
+
 document.getElementById('task1Form').addEventListener('submit', function(event) {
     event.preventDefault();
     const task1Input = document.getElementById('task1Input').value;
     const task1Result = document.getElementById('task1Result');
 
     if (task1Input.toLowerCase() === 'mar') { // Replace 'mar' with the actual answer
-        task1Result.textContent = 'Correct! The letter is: I';
+        task1Result.textContent = 'Riktig! der første ordet er: jeg';
     } else {
-        task1Result.textContent = 'Incorrect answer. Try again.';
+        wrongCounter += 1;
+        if (wrongCounter === 2) {
+            task1Result.textContent = 'Hint: det var 8. Desember 2023';
+        } else if (wrongCounter >= 3) {
+            task1Result.textContent = 'Hint: Svaret er tre bokstaver langt';
+
+        } else {
+            task1Result.textContent = 'Incorrect answer. Try again.';
+        }
     }
 });
