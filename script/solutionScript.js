114f2a775626c0f2b1eaab1d82ced986094aa81b
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-13 19:55:42 +0100

    Changed keyword

--- script/solutionScript.js
+++ script/solutionScript.js
@@ -5,7 +5,10 @@ document.getElementById('solutionForm').addEventListener('submit', function(even
 
     if (solutionInput.toLocaleLowerCase() === 'jeg gleder meg') {
         solutionResult.textContent = 'Riktig! og det jeg gleder meg til er:';
-        solutionResult.textContent = 'page for displaying tickets';
+        setTimeout(() => {
+            window.location.href = 'newPage.html';
+        }, 3000);
+
     } else {
         solutionResult.textContent = 'Ikke helt riktig... det skal være tre ord.';
     }
