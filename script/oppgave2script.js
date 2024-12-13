714386144087587ed79811b6484315f098738670
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-13 21:11:11 +0100

    Matched webpage to script and set right keyword

--- script/oppgave2script.js
+++ script/oppgave2script.js
@@ -1,21 +1,20 @@
 let wrongCounter = 0;
 
-document.getElementById('task1Form').addEventListener('submit', function(event) {
+document.getElementById('task2Form').addEventListener('submit', function(event) {
     event.preventDefault();
-    const task1Input = document.getElementById('task1Input').value;
-    const task1Result = document.getElementById('task1Result');
+    const task2Input = document.getElementById('task2Input').value;
+    const task2Result = document.getElementById('task2Result');
 
-    if (task1Input.toLowerCase() === 'mar') { // Replace 'mar' with the actual answer
-        task1Result.textContent = 'Riktig! der første ordet er: jeg';
+    if (task2Input === '30.10.2023') { // Replace with the correct answer
+        task2Result.textContent = 'Riktig! Det andre ordet er: gleder';
     } else {
         wrongCounter += 1;
         if (wrongCounter === 2) {
-            task1Result.textContent = 'Hint: det var 8. Desember 2023';
+            task2Result.textContent = 'Hint: Sjekk kamerarullen din rundt halloween';
         } else if (wrongCounter >= 3) {
-            task1Result.textContent = 'Hint: Svaret er tre bokstaver langt';
-
-        } else {
-            task1Result.textContent = 'Incorrect answer. Try again.';
+            task2Result.textContent = 'Hint: Det var 2023';
+        } else{
+            task2Result.textContent = "Feil! prøv igjen";
         }
     }
 });
